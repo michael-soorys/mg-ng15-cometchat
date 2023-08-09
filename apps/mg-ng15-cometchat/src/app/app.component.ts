4ba897a4ca104b4e27c7bd5e22af4db559d05477
Michael <michaelsoorys@gmail.com>
2023-08-09 15:16:44 +0300

    Initial commit

---
+++ apps/mg-ng15-cometchat/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "mg-ng15-cometchat-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "mg-ng15-cometchat";
+}
